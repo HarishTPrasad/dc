28864338911533e5a17f0f2b6c03a404bc102d0b
Harry <harish1090prasad@gmail.com>
2025-03-18 17:33:45 +0530

    new updates

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -16,14 +16,3 @@ app.post("/api/submit-form", (req, res) => {
     .catch((err) => res.status(400).json({ error: err.message }));
 });
 
-// Get all users
-router.get("/users", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(400).json({ error: err.message });
-  }
-});
-
-module.exports = router;
